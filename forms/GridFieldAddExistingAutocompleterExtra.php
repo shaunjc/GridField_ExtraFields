7722cd0d62bd1e1c06dd7ff254debd61858d1e27
Shaun Cockerill <shaunjc@users.noreply.github.com>
2017-04-14 15:37:25 +0800

    Update GridFieldAddExistingAutocompleterExtra.php
    Move TODO comment to readme.

--- forms/GridFieldAddExistingAutocompleterExtra.php
+++ forms/GridFieldAddExistingAutocompleterExtra.php
@@ -4,12 +4,6 @@
  * which comes along with most SilverStripe 3 frameworks, except it also has the
  * additional ability to record extra fields when the object is being added to
  * the list.
- * 
- * TODO:
- * Create matching GridField_FormAction, GridFieldDataColumns, GridFieldConfig,
- * and/or GridFieldDetailForm to modify extra fields when objects have already
- * been linked to the Foreign Key, as well as an injector to use the new classes
- * by default.
  */
 class GridFieldAddExistingAutocompleterExtra extends GridFieldAddExistingAutocompleter {
     /**
